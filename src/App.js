fb8ae3152927a0bfc3c46e872c39ee4fadf361b6
James Wienecke <james.a.wienecke@gmail.com>
2022-01-31 10:15:18 -0600

    Following along with tutorial. Implemented Clock component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+// import React
+import {Component} from "react";
+//
+// .Component from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Clock />
   );
 }
 
 export default App;
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>It is now { this.state.date.toLocaleTimeString() }</h1>
+        </div>
+    );
+  }
+}
