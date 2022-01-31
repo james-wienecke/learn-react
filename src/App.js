ea8ae8e103f7094b1d9a612a5ea3149beb423a4d
James Wienecke <james.a.wienecke@gmail.com>
2022-01-31 12:36:42 -0600

    Split Clock component off into its own file. Changed page title in index.html.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-// import React
-import {Component} from "react";
-//
-// .Component from "react";
+import Clock from './Clock.js';
 
 function App() {
   return (
@@ -11,28 +8,3 @@ function App() {
 }
 
 export default App;
-
-class Clock extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000)
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({ date: new Date() });
-  }
-
-  render() {
-    return (
-        <div>
-          <h1>It is now { this.state.date.toLocaleTimeString() }</h1>
-        </div>
-    );
-  }
-}
